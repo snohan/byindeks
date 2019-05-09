c4c52c94c2281f96adf9bb4601690f9448f4a52a
Hansen Snorre <snohan@vegvesen.no>
2019-05-09 14:24:06 +0200

    Startet på kart for Oslo. Lager kall til TD-api.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Byindeks
 Informasjon om formidling av byindeks.
 
-# Bakgrunn
+# Årsrapporter i PDF
 Årsrapporter for byindeksen skal legges ut på vegvesen.no som PDF-dokumenter. Hvert avtaleområde får sin egen rapport, som har Statens vegvesens generelle forside som mal (https://www.vegvesen.no/intranett/Etat/Støttefunksjoner/Visuell+kommunikasjon/Grafiske+maler/Forsider/Generelle+forsider).
 
 I koden blir dette en Rmd-fil per område, som genererer innholdet som MS Word. Alle funksjoner samles i ett felles skript.
@@ -14,13 +14,13 @@ Basert på punktene som inngår i avtalene, og som oppdateres manuelt i koden me
 
 Kart, om nødvendig på flere zoom-nivå, med fargeskala basert på indeksverdi og størrelse basert på ÅDT i basisår.
 
-Bakgrunnskart som viser vegnettet tydelig: OpenStreetMap.
+Bakgrunnskart som viser vegnettet tydelig: Google Roadmap.
 
 ## Liste over punkter
 En enkel tabell med punktene som inneholder navn, vegreferanse og ÅDT i basisår og indeks.
 
 ## Grafisk visning av punktindeksenes årsverdier
-En graf for å vise spredningen av punktenes årsindekser. Vises som et boksplott per år.
+En graf for å vise spredningen av punktenes årsindekser. Vises som et punktplott per år.
 
 ## Kommentarer til resultatene
 En kortfattet tekst som omtaler:
--- get_calls_from_trafficdata_api.R
+++ get_calls_from_trafficdata_api.R
@@ -1,5 +1,85 @@
-# Henter info fra Trafikkdata-API.
+# Fetching data from Trafikkdata-API.
 
-# Hent punkter ####
+# Packages ####
+library(ghql)
+library(jsonlite)
+library(httr)
+library(tidyverse)
+library(lubridate)
+
+# Functions ####
+
+cli <- GraphqlClient$new(
+  url = "https://www.vegvesen.no/trafikkdata/api/?query="
+)
+
+getPoints <- function() {
+  # Get all traffic registration points
+  query_points <-
+    "query allPoints{
+  trafficRegistrationPoints{
+    id
+    name
+    trafficRegistrationType
+    location{
+      coordinates{
+        latLon{
+          lat
+          lon
+        }
+      }
+      roadReferences{
+        validFrom
+        validTo
+        roadReference532{
+          shortForm
+        }
+      }
+    }
+  }
+}"
+
+  myqueries <- Query$new()
+  myqueries$query("points", query_points)
+
+  points <- cli$exec(myqueries$queries$points) %>%
+    fromJSON(simplifyDataFrame = T, flatten = T) %>%
+    as.data.frame() %>%
+    rename(point_id = data.trafficRegistrationPoints.id,
+           point_name = data.trafficRegistrationPoints.name,
+           traffic_type =
+             data.trafficRegistrationPoints.trafficRegistrationType)
+
+  return(points)
+}
+
+# test
+points <- getPoints() %>%
+  unnest()
+
+# Get points ####
+query allPoints{
+  trafficRegistrationPoints{
+    id
+    name
+    location{
+      coordinates{
+        latLon{
+          lat
+          lon
+        }
+      }
+      roadReferences{
+        validFrom
+        validTo
+        roadReference532{
+          shortForm
+        }
+      }
+    }
+  }
+}
+
+
 
 # Hent ÅDT ####
