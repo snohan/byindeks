186b62c090f1a55fc0ef65c8c9ef133206a929d1
Hansen Snorre <snohan@vegvesen.no>
2019-07-23 13:38:24 +0200

    Rydding i filer. Arkivering av gamle filer. Ignorerer alle resultatfiler.

---
+++ rmd_setup.R
@@ -0,0 +1,32 @@
+# Setting up Rmd knitr options and map essentials
+knitr::opts_chunk$set(echo = FALSE, warning = FALSE, message = FALSE,
+                      error = FALSE, cache = TRUE)
+
+nvdb_map_url <- "https://nvdbcache.geodataonline.no/arcgis/rest/services/Trafikkportalen/GeocacheTrafikkJPG/MapServer/tile/{z}/{y}/{x}"
+
+nvdb_map_attribution <- "NVDB, Geovekst, kommunene og Open Street Map contributors (utenfor Norge)"
+
+nvdb_crs <- leafletCRS(crsClass = "L.Proj.CRS", code = "EPSG:25833",
+                       proj4def = "+proj=utm +zone=33 +ellps=GRS80 +units=m +no_defs",
+                       #scales = 1250*2^(16:0),
+                       resolutions = c(
+                         21674.7100160867,
+                         10837.35500804335,
+                         5418.677504021675,
+                         2709.3387520108377,
+                         1354.6693760054188,
+                         677.3346880027094,
+                         338.6673440013547,
+                         169.33367200067735,
+                         84.66683600033868,
+                         42.33341800016934,
+                         21.16670900008467,
+                         10.583354500042335,
+                         5.291677250021167,
+                         2.6458386250105836,
+                         1.3229193125052918,
+                         0.6614596562526459,
+                         0.33072982812632296,
+                         0.16536491406316148
+                       ),
+                       origin = c(-2500000.0, 9045984.0))
