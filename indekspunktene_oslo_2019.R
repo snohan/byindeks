7542f0d4add8591006fb818b9d4bb4cb65d6e2e7
Hansen Snorre <snohan@vegvesen.no>
2019-07-23 09:40:38 +0200

    SVV map in Leaflet

--- indekspunktene_oslo_2019.R
+++ indekspunktene_oslo_2019.R
@@ -7,11 +7,13 @@ points <- getPoints()
 # join points
 oslopunkter <- read_csv2("byindeks_trp.csv") %>%
   dplyr::filter(city_area_name == "Oslo og Akershus",
-                agreement_start == 2019)
+                agreement_start == 2019) %>%
+  dplyr::mutate(established = "Ja" )
 
-indekspunktene_oslo <- dplyr::left_join(oslopunkter, points)
+indekspunkter_oslo <- dplyr::left_join(oslopunkter, points) %>%
+  dplyr::select(1:5, 7:11, 6)
 
-write.csv2(indekspunktene_oslo, file = "indekspunktene_oslo.csv",
+write.csv2(indekspunkter_oslo, file = "indekspunktene_oslo.csv",
            row.names = F)
 
 
@@ -19,7 +21,7 @@ write.csv2(indekspunktene_oslo, file = "indekspunktene_oslo.csv",
 oslo_adt <- getAdtForpoints(indekspunktene_oslo$trp_id)
 # TODO: filtrere ut 2018
 # TODO: join
-# TODO: hente fra NVDB de so mangler?
+# TODO: hente fra NVDB de som mangler?
 
 # TEst
 #uten_adt <- getTrpAadt("32135V604101")
