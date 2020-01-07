71786fe70e8f40a1f07b2f4f56bbbbe5ad4ddbd6
Hansen Snorre <snohan@vegvesen.no>
2020-01-07 13:39:35 +0100

    Rmd file for Tromsø. Function for creating point adt map.

--- index_report_functions.R
+++ index_report_functions.R
@@ -1,5 +1,37 @@
 #
 
+create_point_adt_map <- function(all_point_info_df) {
+  palett_adt <-
+    colorNumeric(palette = "Greens",
+                 domain = NULL)
+
+  point_adt_map <- all_point_info_df %>%
+    leaflet(width = "100%",
+            options = leafletOptions(crs = nvdb_crs,
+                                     zoomControl = F)) %>%
+    addTiles(urlTemplate = nvdb_map_url,
+             attribution = nvdb_map_attribution) %>%
+    addCircleMarkers(
+      radius = 6,
+      stroke = T,
+      weight = 2,
+      color = "#444f55",
+      opacity = 0.8,
+      fill = T,
+      fillColor = ~palett_adt(adt),
+      fillOpacity = 0.8
+    ) %>%
+    addLegend("bottomright",
+              pal = palett_adt,
+              values = ~adt,
+              title = "ÅDT",
+              opacity = 0.7,
+              labFormat = labelFormat(big.mark = " "))
+
+  return(point_adt_map)
+}
+
+
 create_pointindex_map <- function(all_point_info_df) {
   # Create a red-green scale based on index values
   negative_value <- round(abs(min(all_point_info_df$index, na.rm = T)), digits = 0) + 1
