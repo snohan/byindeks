8fe107081e812d44bf727246c20088eb874afd81
Hansen Snorre <snorre.hansen@vegvesen.no>
2020-12-14 14:10:33 +0100

    Revised text on traffic situations

--- index_report_functions.R
+++ index_report_functions.R
@@ -45,7 +45,7 @@ create_point_adt_map <- function(all_point_info_df) {
       fillColor = ~palett_adt(adt),
       fillOpacity = 0.8
     ) %>%
-    addLegend("bottomleft",
+    addLegend("bottomright",
               pal = palett_adt,
               values = ~adt,
               title = "ADT",
