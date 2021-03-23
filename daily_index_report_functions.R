4899c1c0b47b5af38450f4cfe4cfb8481848a0ae
Snorre Hansen <snorre.hansen@vegvesen.no>
2021-03-23 12:59:39 +0100

    Daily city index with two normals

--- daily_index_report_functions.R
+++ daily_index_report_functions.R
@@ -1,5 +1,34 @@
 # Functions for use in reports on daily index
 
+make_daily_plot <- function(area_index) {
+
+  area_index %>%
+    dplyr::mutate(periode = paste0("Uke \n ", ukenr)) %>%
+    dplyr::mutate(periode = factor(periode,
+                                   levels = stringr::str_sort(periode, numeric = TRUE))) %>%
+    ggplot2::ggplot(aes(ukedag, index)) +
+    geom_bar(aes(fill = index > 0), stat = "identity",
+             color = "#000000",
+             alpha = 0.6) +
+    scale_fill_manual(guide = FALSE, breaks = c(FALSE, TRUE),
+                      values = c("#ed1c2e", "#58b02c")) +
+    geom_hline(yintercept = 0, color = "#000000") +
+    facet_grid(periode ~., switch = "y") +
+    scale_y_continuous(position = "right",
+                       limits = c(-50, 50),
+                       breaks = c(-20, 0, 200)) +
+    xlab("") +
+    ylab("Endring i trafikkmengde (%)\n") +
+    ggtitle(label = "Daglig endring i trafikkmengde i 2021",
+            subtitle = "Sammenlignet med normalperioden 2016-2019") +
+    theme(strip.text.y = element_text(angle = 180),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(),
+          strip.background = element_blank()) +
+    labs(caption = "Data: Statens vegvesen")
+}
+
 # Function for generating table
 # make_border_table <- function(border_base, caption_text) {
 #
@@ -83,7 +112,7 @@ make_daily_border_plot <- function(area_index, subtitle_text) {
 }
 
 
-make_daily_dodged_bar_plot <- function(area_index, subtitle_text) {
+make_daily_dodged_bar_plot <- function(area_index, subtitle_text, x_min, x_max) {
 
   area_index %>%
     dplyr::mutate(periode = paste0("Uke \n ", ukenr)) %>%
@@ -100,19 +129,20 @@ make_daily_dodged_bar_plot <- function(area_index, subtitle_text) {
     geom_hline(yintercept = 0, color = "#000000") +
     facet_grid(periode ~., switch = "y") +
     scale_y_continuous(position = "right",
-                       limits = c(-100, 100),
-                       breaks = c(-80, 0, 80)) +
+                       limits = c(x_min - 20, x_max + 20),
+                       breaks = c(x_min, 0, x_max)) +
     xlab("") +
     ylab("Endring i trafikkmengde (%)\n") +
-    ggtitle(label = "Daglig endring i trafikkmengde over riksgrensen i 2021",
+    ggtitle(label = "Daglig endring i trafikkmengde i 2021",
             subtitle = subtitle_text) +
     theme(strip.text.y = element_text(angle = 180),
+          axis.text.y = element_text(size = 7),
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(),
           panel.background = element_blank(),
           strip.background = element_blank(),
           legend.position = "bottom") +
-    labs(caption = "Data: Statens vegvesen")
+    labs(caption = "Data: Statens vegvesen og fylkeskommunene")
 }
 
 
@@ -141,6 +171,9 @@ calculate_daily_point_index <- function(dt_table, normal_days) {
                                 digits = 1),
                   index = dplyr::na_if(index, "Inf")) %>%
     dplyr::left_join(norske_ukedager) %>%
-    dplyr::select(trp_id = point_id, ukedag, ukenr = weekno, total_volume, daily_traffic, index) %>%
+    dplyr::select(trp_id = point_id, area_name,
+                  ukedag, ukenr = weekno, total_volume, daily_traffic, index) %>%
     dplyr::arrange(factor(ukedag, levels = ukedager))
 }
+
+
--- normal_days.R
+++ normal_days.R
@@ -56,7 +56,8 @@
 # 2020
 
 day_of_week_traffic_2020 <- get_trp_average_day_of_week_traffic_by_month_for_a_year_for_trp_list(
-  border_trp_ids,
+  #border_trp_ids,
+  trps$trp_id,
   2020
   )
 
@@ -84,5 +85,59 @@ day_of_week_traffic_average_2020 <- dplyr::bind_rows(
 
 # write normal days to csv, and read it in when running report to save time etc.
 write.csv2(day_of_week_traffic_average_2020,
-           file = "border_normal_days_2020.csv",
+           #file = "border_normal_days_2020.csv",
+           file = "city_normal_days_2020.csv",
            row.names = F)
+
+
+# Normal daily traffic per month per trp ####
+
+# day_of_week_traffic_2016 <- get_trp_average_day_of_week_traffic_by_month_for_a_year_for_trp_list(
+#   trps$trp_id,
+#   2016
+#   )
+#
+# day_of_week_traffic_2017 <- get_trp_average_day_of_week_traffic_by_month_for_a_year_for_trp_list(
+#   trps$trp_id,
+#   2017
+#   )
+#
+# day_of_week_traffic_2018 <- get_trp_average_day_of_week_traffic_by_month_for_a_year_for_trp_list(
+#   trps$trp_id,
+#   2018
+#   )
+#
+# day_of_week_traffic_2019 <- get_trp_average_day_of_week_traffic_by_month_for_a_year_for_trp_list(
+#   trps$trp_id,
+#   2019
+#   )
+#
+# day_of_week_traffic_average <- dplyr::bind_rows(
+#   day_of_week_traffic_2016,
+#   day_of_week_traffic_2017,
+#   day_of_week_traffic_2018,
+#   day_of_week_traffic_2019
+#   ) %>%
+#   dplyr::group_by(trp_id, month, day_name) %>%
+#   dplyr::summarise(
+#     daily_traffic = round(mean(average_day_of_week_traffic),
+#                           digits = 0),
+#     included_days = sum(included_days),
+#     possible_days = sum(possible_days)
+#   ) %>%
+#   dplyr::mutate(
+#     day_number_of_week = dplyr::case_when(
+#       day_name == "MONDAY" ~ 1,
+#       day_name == "TUESDAY" ~ 2,
+#       day_name == "WEDNESDAY" ~ 3,
+#       day_name == "THURSDAY" ~ 4,
+#       day_name == "FRIDAY" ~ 5,
+#       day_name == "SATURDAY" ~ 6,
+#       day_name == "SUNDAY" ~ 7
+#     )
+#   )
+
+# write normal days to csv, and read it in when running report to save time etc.
+# write.csv2(day_of_week_traffic_average,
+#            file = "city_normal_days.csv",
+#            row.names = F)
