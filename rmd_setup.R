bf4bf93275f42a9a2b942230b4347576b48f8022
Snorre Hansen <snorre.hansen@vegvesen.no>
2024-03-07 09:41:30 +0100

    Traffic links and BC in city.

--- rmd_setup.R
+++ rmd_setup.R
@@ -20,6 +20,7 @@ library(scales)
 
 source("H:/Programmering/R/byindeks/index_report_functions.R")
 source("H:/Programmering/R/byindeks/split_road_system_reference.R")
+source("H:/Programmering/R/byindeks/leaflet_nvdb_map_setup.R")
 
 svv_background_color <- "#F5F5F5"
 
@@ -34,38 +35,6 @@ knitr::opts_chunk$set(
   dev = "ragg_png",
   tab.topcaption = TRUE
   )
-
-# Map essentials ####
-nvdb_map_url <-
-  "https://nvdbcache.geodataonline.no/arcgis/rest/services/Trafikkportalen/GeocacheTrafikkJPG/MapServer/tile/{z}/{y}/{x}"
-
-nvdb_map_attribution <-
-  "NVDB, Geovekst, kommunene og Open Street Map contributors (utenfor Norge)"
-
-nvdb_crs <- leafletCRS(
-  crsClass = "L.Proj.CRS", code = "EPSG:25833",
-  proj4def = "+proj=utm +zone=33 +ellps=GRS80 +units=m +no_defs",
-  resolutions = c(
-    21674.7100160867,
-    10837.35500804335,
-    5418.677504021675,
-    2709.3387520108377,
-    1354.6693760054188,
-    677.3346880027094,
-    338.6673440013547,
-    169.33367200067735,
-    84.66683600033868,
-    42.33341800016934,
-    21.16670900008467,
-    10.583354500042335,
-    5.291677250021167,
-    2.6458386250105836,
-    1.3229193125052918,
-    0.6614596562526459,
-    0.33072982812632296,
-    0.16536491406316148
-  ),
-  origin = c(-2500000.0, 9045984.0))
 
 # Functions
 decimal_comma <- function(number) {
--- traffic_network.R
+++ traffic_network.R
@@ -1,35 +1,96 @@
+# Prepare a traffic link graph for map viewing
 {
   library(tidyverse)
   library(sf)
   library(sfnetworks)
   library(igraph)
-  #library(tidygraph)
+  library(tidygraph)
 }
 
-# Read ----
-# Geojson files from ADM
-directed_links <-
-  sf::st_read("C:/Users/snohan/Desktop/directed_traffic_links_2023.geojson") |>
-  dplyr::select(
-    id,
-    isTrafficWithMetering,
-    length,
-    primaryTrpId,
-    lowestSpeedLimit,
-    startTrafficNodeId,
-    endTrafficNodeId,
-    trafficVolumes,
-    municipalities,
-    roadCategory,
-    functionalRoadClasses,
-    functionClasses
+# Choose
+trd_kommuner <-
+  c(
+    5001,
+    5031,
+    5035,
+    5028,
+    5029,
+    5059
   )
 
+# Read ----
+## Links ----
+# Geojson files from ADM
+test_directed_links <-
+  sf::st_read(
+    "C:/Users/snohan/Desktop/directed_traffic_links_2023.geojson",
+    query =
+      "SELECT *
+      FROM \"directed_traffic_links_2023\"
+      LIMIT 10"
+  )
+
+directed_links <-
+  sf::st_read(
+    "C:/Users/snohan/Desktop/directed_traffic_links_2023.geojson",
+    query =
+      "SELECT
+          id, parentTrafficLinkId, length, primaryTrpId, lowestSpeedLimit, isFerryTrafficLink,
+          hasOnlyPublicTransportLanes, isBlocked,
+          startTrafficNodeId, endTrafficNodeId, municipalities, roadSystemReferences,
+          functionalRoadClasses, functionClasses
+      FROM \"directed_traffic_links_2023\""
+    # trafficVolumes
+    # isTrafficWithMetering
+    # roadCategory
+  ) |>
+  dplyr::rename(
+    trp_id = primaryTrpId,
+    from = startTrafficNodeId,
+    to = endTrafficNodeId
+  ) |>
+  tidyr::unnest_longer(
+    municipalities,
+    values_to = "municipality_id"
+  ) |>
+  dplyr::filter(
+    municipality_id %in% trd_kommuner,
+    hasOnlyPublicTransportLanes == FALSE,
+    isBlocked == FALSE
+  ) |>
+  # Need to remove duplicate links (crossing municipality boundaries)
+  dplyr::select(-municipality_id) |>
+  dplyr::distinct() |>
+  dplyr::mutate(
+    speed =
+      dplyr::case_when(
+        isFerryTrafficLink == TRUE ~ 30,
+        TRUE ~ lowestSpeedLimit
+      ),
+    travel_time_minutes = length / (speed * (1000 / 60))
+  ) |>
+  dplyr::select(
+    id,
+    from, to,
+    length,
+    trp_id,
+    roadSystemReferences,
+    functionalRoadClasses,
+    functionClasses,
+    travel_time_minutes,
+    geometry
+  ) |>
+  sf::st_as_sf()
+
+
+## Nodes ----
 nodes <-
-  sf::st_read("C:/Users/snohan/Desktop/traffic-nodes-2023.geojson")
-
-
-
+  sf::st_read("C:/Users/snohan/Desktop/traffic-nodes-2023.geojson") |>
+  # Trim nodes to the ones in area
+  dplyr::filter(
+    id %in% unique(c(directed_links$from, directed_links$to))
+  ) |>
+  dplyr::select(id)
 
 
 # Directed graph ----
@@ -43,55 +104,163 @@ nodes <-
 
 directed_graph <-
   sfnetworks::sfnetwork(
-    nodes = nodes_chosen,
-    edges = links_directed,
+    nodes = nodes,
+    edges = directed_links,
     directed = TRUE,
-    node_key = "node_id",
-    edges_as_lines = FALSE,
-    force = FALSE
-  )
-
-plot(directed_graph)
-
-ggplot2::autoplot(directed_graph)
-
-# Verify that directed graph contains one single component
-directed_graph |> igraph::count_components(mode="weak")
-
-cc <- directed_graph |> igraph::components(mode="weak")
-
-rev(table(cc$csize))
-
-## Compute edge betweenness centrality ----
-directed_graph_bc <-
-  directed_graph %>%
+    node_key = "id",
+    edges_as_lines = TRUE,
+    # Will error if force = FALSE and more than one component
+    force = TRUE
+  ) |>
   tidygraph::activate("edges") |>
   dplyr::mutate(
     bc_travel_time =
       tidygraph::centrality_edge_betweenness(
         weights = travel_time_minutes,
         directed = TRUE,
-        cutoff = NULL)
+        cutoff = NULL
+      )
   )
 
-# Extract link metainfo, now including BC
-# TODO: And reattaching geometry
-edges_main <-
-  directed_graph_bc |>
+links <-
+  directed_graph |>
   tidygraph::activate("edges") |>
   tibble::as_tibble()
 
+base::class(directed_graph)
+base::length(directed_graph)
 
 
+ggplot2::autoplot(directed_graph)
 
-# graph_test <-
-#   tidygraph::tbl_graph(
-#     nodes = nodes_chosen,
-#     node_key = "node_id",
-#     edges = links_directed_nodes
-#   )
-#
-# test <- igraph::graph(c(links_directed_nodes$from, links_directed_nodes$to))
+# directed_graph |>
+#   ggraph::ggraph() +
+#     #geom_edge_sf()
+#     geom_edge_sf(aes(alpha = bc_travel_time))
+
+# Verify that graph contains one single component
+cc <-
+  directed_graph |>
+  igraph::components(mode="weak")
+
+cc$csize
 
 
+## Smoothing out pseudo nodes? (I-kryss)
+
+
+# Edited graph
+edges_to_remove <-
+  c(
+    "1018242714-WITH",
+    "1018242714-AGAINST"
+  )
+
+
+directed_links_2 <-
+  directed_links |>
+  dplyr::filter(
+    !(id %in% edges_to_remove)
+  )
+
+directed_graph_2 <-
+  sfnetworks::sfnetwork(
+    nodes = nodes,
+    edges = directed_links_2,
+    directed = TRUE,
+    node_key = "id",
+    edges_as_lines = TRUE,
+    # Will error if force = FALSE and more than one component
+    force = TRUE
+  ) |>
+  tidygraph::activate("edges") |>
+  dplyr::mutate(
+    bc_travel_time_2 =
+      tidygraph::centrality_edge_betweenness(
+        weights = travel_time_minutes,
+        directed = TRUE,
+        cutoff = NULL
+      )
+  )
+
+
+# Pull out BC
+bc_1 <-
+  directed_graph |>
+  tidygraph::activate("edges") |>
+  tibble::as_tibble() |>
+  sf::st_drop_geometry() |>
+  dplyr::select(
+    id,
+    bc_travel_time
+  )
+
+bc_2 <-
+  directed_graph_2 |>
+  tidygraph::activate("edges") |>
+  tibble::as_tibble() |>
+  sf::st_drop_geometry() |>
+  dplyr::select(
+    id,
+    bc_travel_time_2
+  )
+
+bc_delta <-
+  bc_1 |>
+  dplyr::left_join(
+    bc_2,
+    by = dplyr::join_by(id)
+  ) |>
+  dplyr::mutate(
+    delta_bc = bc_travel_time_2 - bc_travel_time
+  ) |>
+  dplyr::select(
+    id, delta_bc
+  )
+
+bc_delta |>
+  dplyr::filter(
+    delta_bc != 0
+  ) |>
+  nrow()
+
+
+directed_links_bc_delta <-
+  directed_links |>
+  dplyr::left_join(
+    bc_delta,
+    by = dplyr::join_by(id)
+  )
+
+directed_graph_bc_delta <-
+  sfnetworks::sfnetwork(
+    nodes = nodes,
+    edges = directed_links_bc_delta,
+    directed = TRUE,
+    node_key = "id",
+    edges_as_lines = TRUE,
+    # Will error if force = FALSE and more than one component
+    force = TRUE
+  ) |>
+  tidygraph::activate("edges") |>
+  dplyr::mutate(
+    bc_travel_time =
+      tidygraph::centrality_edge_betweenness(
+        weights = travel_time_minutes,
+        directed = TRUE,
+        cutoff = NULL
+      ),
+    ln_bc = log(bc_travel_time + 1),
+    link_label =
+      paste(id, roadSystemReferences, bc_travel_time, delta_bc, sep = "<br/>") |>
+      purrr::map(htmltools::HTML)
+    #link_label = purrr::map(link_label, htmltools::HTML)
+  )
+
+
+## write
+readr::write_rds(
+  directed_graph_bc_delta,
+  "trd_graf.rds"
+)
 
