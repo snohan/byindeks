7c28ca4fd08e9d93071a33a5d432506410c04276
Snorre Hansen <snorre.hansen@vegvesen.no>
2024-11-01 15:02:18 +0100

    City updates 24-10.

--- city_index_dataprep.R
+++ city_index_dataprep.R
@@ -59,7 +59,7 @@ trp_id_msnr <-
 {
 present_year <- 2024
 index_month <- 10 # the one to be published now
-city_number <- 1952
+city_number <- 956
 }
 # End choose
 
@@ -717,7 +717,7 @@ trp_not_ok <-
 # TODO: Shiny app for checking MDT
 
 mdt_validated |>
-  dplyr::filter(trp_id %in% trp_mdt_ok_refyear[21:22]) |>
+  dplyr::filter(trp_id %in% trp_mdt_ok_refyear[28:29]) |>
   dplyr::select(
     trp_id,
     year,
