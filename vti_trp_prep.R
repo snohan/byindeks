e7852c38f4b960caddc35f86064512ad91ae9677
Snorre Hansen <snorre.hansen@vegvesen.no>
2025-02-19 14:32:15 +0100

    New VTI TRP update of method.

--- vti_trp_prep.R
+++ vti_trp_prep.R
@@ -18,20 +18,35 @@
 
 # Get data ----
 ## Counties ----
-counties <- get_counties()
+counties <-
+  get_counties() |>
+  dplyr::rename(
+    county_id = county_number,
+    county_geono = geo_number
+  )
+# Should use county_geono throughout
 
 
 ## TRP candidates ----
-trps_last_year <- get_published_pointindex_for_months_paginated(962, 2024, 12)[[1]]
+# Fetch once and save
+{
+  trps_last_year <- get_published_pointindex_for_months_paginated(962, 2024, 12)[[1]]
 
-# Fetch once and save?
-# readr::write_rds(
-#   trps_last_year,
-#   file = "new_vti/trps_last_year.rds"
-# )
+  readr::write_rds(
+    trps_last_year,
+    file = "new_vti/trps_last_year.rds"
+  )
+}
+# Read back in
+trps_last_year <-
+  readr::read_rds(
+    file = "new_vti/trps_last_year.rds"
+  )
 
 trp_latest_data <- get_trps_latest_data()
 
+# NB! Do not filter anything more here as we need to catch those used last year (some may be lost in subsequent trp dfs)
+# Supposedly none have changed registration_frequency
 trp_for_vti <-
   get_points() |>
   dplyr::group_by(trp_id) |>
@@ -39,8 +54,7 @@ trp_for_vti <-
   dplyr::ungroup() |>
   dplyr::filter(
     traffic_type == "VEHICLE",
-    registration_frequency == "CONTINUOUS",
-    operational_status == "OPERATIONAL"
+    registration_frequency == "CONTINUOUS"
   ) |>
   split_road_system_reference() |>
   dplyr::left_join(
@@ -69,19 +83,7 @@ trp_for_vti <-
         road_category == "E" ~ "R",
         road_category == "R" ~ "R",
         road_category == "F" ~ "F"
-      ),
-    used_last_year =
-      dplyr::case_when(
-        trp_id %in% trps_last_year ~ TRUE,
-        TRUE ~ FALSE
       )
-  ) |>
-  dplyr::filter(
-    road_category %in% c("F", "R"),
-    valid_from < "2024-01-01",
-    latest_day > "2025-02-01"
-    #stringr::str_detect(road_reference, "SD", negate = TRUE),
-    #stringr::str_detect(road_reference, "KD", negate = TRUE)
   )
 
 
@@ -98,6 +100,7 @@ readr::write_rds(
 
 remove(trp_aadt_raw)
 
+# Setting some criterias to filter out real candidates for next year
 trp_aadts <-
   readr::read_rds("trp_aadt.rds") |>
   dplyr::filter(
@@ -172,6 +175,13 @@ links <-
   )
 
 
+# Some links may be missing function class
+links_na <-
+  links |>
+  dplyr::filter(
+    is.na(function_class)
+  )
+
 ### County ID ----
 # Keep only one county id for border crossing ones
 link_county_id <-
@@ -183,6 +193,7 @@ link_county_id <-
   ) |>
   dplyr::rowwise() |>
   dplyr::mutate(
+    # Just picking the one at the top (random?)
     county_id_single = purrr::pluck(countyIds, 1, 1)
   ) |>
   dplyr::ungroup() |>
@@ -223,13 +234,14 @@ traffic_work_per_link <-
 
 
 ## TRP on link ----
-# All links and TRP ids
+# All links, function class and TRP ids
 link_trp_id <-
   links |>
   sf::st_drop_geometry() |>
   dplyr::select(
     id,
-    associatedTrpIds
+    associatedTrpIds,
+    function_class
   ) |>
   dplyr::filter(
     !(associatedTrpIds == "[ ]")
@@ -243,6 +255,7 @@ link_trp_id <-
   dplyr::select(
     -associatedTrpIds
   ) |>
+  # Filter by candidates based on available data
   dplyr::filter(
     trp_id %in% trp_aadts$trp_id
   ) #|>
@@ -257,9 +270,32 @@ link_trp_id <-
 ## Tidy TRPs ----
 trp_for_vti_tidy <-
   trp_for_vti |>
+  # Filter by available data (very generally)
+  dplyr::filter(
+    operational_status == "OPERATIONAL",
+    road_category %in% c("F", "R"),
+    valid_from < "2024-01-01",
+    latest_day > "2025-02-15"
+    #stringr::str_detect(road_reference, "SD", negate = TRUE),
+    #stringr::str_detect(road_reference, "KD", negate = TRUE)
+  ) |>
+  # Filter by available data (more specifically)
   dplyr::inner_join(
     trp_aadts,
     by = "trp_id"
+  ) |>
+  # Add link id and function class
+  dplyr::left_join(
+    link_trp_id,
+    by = dplyr::join_by(trp_id)
+  ) |>
+  dplyr::mutate(
+    # Before looking at candidate TRPs, it is either to keep (from last year) or a candidate (maybe add)
+    use_in_vti =
+      dplyr::case_when(
+        trp_id %in% trps_last_year ~ "keep",
+        TRUE ~ "candidate"
+      ),
   ) |>
   dplyr::select(
     trp_id,
@@ -270,10 +306,9 @@ trp_for_vti_tidy <-
     road_category,
     road_category_and_number,
     road_reference,
-    # TODO: function class?
-    #road_link_position,
+    function_class,
     lat, lon,
-    used_last_year,
+    use_in_vti,
     adt
   ) |>
   dplyr::arrange(
@@ -303,18 +338,20 @@ links_tidy <-
     !is.na(traffic_work_km)
   ) |>
   dplyr::left_join(
-    # Will duplicate some links?
-    link_trp_id,
+    # Will duplicate some links
+    link_trp_id |>
+      dplyr::select(-function_class),
     by = join_by(id)
   ) |>
   dplyr::left_join(
     counties,
-    by = join_by(county_id == county_number)
+    by = join_by(county_id)
   ) |>
   dplyr::select(
     id,
     county_id,
-    county_geonumber = geo_number,
+    county_geono,
+    county_name,
     road_category,
     roadSystemReferences,
     function_class,
@@ -332,11 +369,6 @@ remove(links)
 
 # Read back in ----
 # If you are back another day...
-trps_last_year <-
-  readr::read_rds(
-    file = "new_vti/trps_last_year.rds"
-  )
-
 trp_for_vti_tidy <-
   readr::read_rds(
     file = "new_vti/trp_for_vti_tidy.rds"
@@ -351,11 +383,14 @@ links_tidy <-
 # TRP chosen ----
 # Less work:
 # Would like to keep all TRPs from last year, except a few: used_last_year_but_discard
-# Would like to not add any new ones, except a few: not_used_last_year_but_add
+# Would like to not add any new ones, except a few: new_ones_to_add
+# These are specified in "unwanted_vti_trp_2025.R"
+
 # Classify all TRPs as either one of
 # - keep
 # - discard
 # - add
+# - candidate (left as candidates)
 
 # Look at map and decide on which new ones to add
 {
@@ -365,11 +400,19 @@ trp_for_vti_chosen <-
   trp_for_vti_tidy |>
   dplyr::filter(
     !(trp_id %in% used_last_year_but_discard),
-    !(trp_id %in% new_ones_not_to_add$trp_id)
+    !(trp_id %in% declined_candidates$trp_id)
   ) |>
   dplyr::mutate(
-    trp_label = paste(name, road_category_and_number, adt, sep = "<br/>"),
-    # TODO: function_class?
+    # After having considered candidates by looking at map by county, status can change
+    # keep <- (discard, keep)
+    # candidate <- (add, candidate)
+    use_in_vti =
+      dplyr::case_when(
+        #trp_id %in% used_last_year_but_discard ~ "discard",
+        trp_id %in% new_ones_to_add ~ "add",
+        TRUE ~ use_in_vti
+      ),
+    trp_label = paste(name, road_category_and_number, function_class, adt, sep = "<br/>"),
     trp_label = lapply(trp_label, htmltools::HTML)
   )
 
@@ -381,16 +424,10 @@ readr::write_rds(
 
 
 # OUTPUT#3 TRPs ----
+# Can not just use trp_for_vti_chosen here as some of the ones used last year may have been lost in all the filtering of candidates for next year
+# These need not be shown in map, but they must be in the list which is being used to update the selection in the GUI
+# keep and add are complete, but discard must be complemented by looking at the list of those used last year
 {
-trps_last_year_to_keep <-
-  trp_for_vti_chosen |>
-  dplyr::filter(
-    trp_id %in% trps_last_year
-  ) |>
-  dplyr::mutate(
-    change_status = "keep"
-  )
-
 trps_last_year_to_discard <-
   tibble::tibble(
     trp_id = trps_last_year
@@ -405,43 +442,31 @@ trps_last_year_to_discard <-
   dplyr::select(
     county_geono,
     county_name,
+    municipality_name,
     trp_id,
     name,
-    road_reference
-  ) |>
-  dplyr::arrange(
-    county_geono,
-    name
+    road_category_and_number
   ) |>
   dplyr::mutate(
-    change_status = "discard"
-  )
-
-trps_to_add <-
-  trp_for_vti_chosen |>
-  dplyr::filter(
-    !(trp_id %in% trps_last_year)
-  ) |>
-  dplyr::mutate(
-    change_status = "add"
+    use_in_vti = "discard"
   )
 
 ## Write
 dplyr::bind_rows(
-  trps_last_year_to_keep,
-  trps_last_year_to_discard,
-  trps_to_add
+  trp_for_vti_chosen |>
+    dplyr::select(
+      county_geono,
+      county_name,
+      municipality_name,
+      trp_id,
+      name,
+      road_category_and_number,
+      use_in_vti
+    ),
+  trps_last_year_to_discard
 ) |>
-  dplyr::select(
-    trp_id,
-    name,
-    county_name,
-    municipality_name,
-    road_category_and_number,
-    change_status
-  ) |>
   dplyr::arrange(
-    change_status,
+    use_in_vti,
     name
   ) |>
   writexl::write_xlsx(
@@ -452,19 +477,36 @@ dplyr::bind_rows(
 
 # OUTPUT#1 Links ----
 {
+  # The ones we want to see in map
+  trps_chosen_but_not_the_discarded <-
+    trp_for_vti_chosen |>
+    dplyr::filter(
+      !(use_in_vti == "discard"),
+      !(trp_id %in% declined_candidates$trp_id)
+    ) |>
+    dplyr::select(
+      trp_id,
+      use_in_vti
+    )
+
   links_chosen <-
     links_tidy |>
     dplyr::mutate(
       # Need to keep all links in order to calculate traffic work ratios
+      # Retain only trp_ids that are keep, add or candidate
       trp_id =
         dplyr::case_when(
-          trp_id %in% trp_for_vti_chosen$trp_id ~ trp_id,
+          trp_id %in% trps_chosen_but_not_the_discarded$trp_id ~ trp_id,
           TRUE ~ NA_character_
-        ),
-      to_keep = dplyr::case_when(
-        trp_id %in% trps_last_year_to_keep$trp_id ~ TRUE,
-        TRUE ~ FALSE
-      )
+        )#,
+      #to_keep = dplyr::case_when(
+      #  trp_id %in% trps_last_year_to_keep$trp_id ~ TRUE,
+      #  TRUE ~ FALSE
+      #)
+    ) |>
+    dplyr::left_join(
+      trps_chosen_but_not_the_discarded,
+      by = dplyr::join_by(trp_id)
     )
 
   readr::write_rds(
@@ -476,72 +518,88 @@ dplyr::bind_rows(
 
 # OUTPUT#2 Summary ----
 # Measured traffic work
+# Traffic work stats
+{
+traffic_work_stats <-
+  traffic_work_per_link |>
+  dplyr::left_join(
+    links_tidy |>
+      sf::st_drop_geometry() |>
+      dplyr::select(
+        id,
+        county_geono,
+        road_category
+      ),
+    by = join_by(id)
+  ) |>
+  dplyr::summarise(
+    traffic_work_mill_km = sum(traffic_work_km) / 1e6,
+    .by = c(county_geono, road_category)
+  ) |>
+  dplyr::select(
+    county_geono,
+    road_category,
+    traffic_work_mill_km
+  ) |>
+  dplyr::arrange(
+    county_geono
+  )
+
+readr::write_rds(
+  traffic_work_stats,
+  file = "new_vti/traffic_work_2023.rds"
+)
+
 measured_traffic_work <-
   links_chosen |>
   sf::st_drop_geometry() |>
+  dplyr::filter(
+    !is.na(trp_id)
+  ) |>
   dplyr::summarise(
-    measured_traffic_work_mill_km = sum(aadt * 365 * length) / 1e9,
-    .by = c(county_id, road_category)
+    measured_traffic_work_mill_km = sum(traffic_work_km) / 1e6,
+    .by = c(county_geono, road_category)
   ) |>
   dplyr::left_join(
-    traffic_work,
-    by = c("county_id", "road_category")
+    traffic_work_stats,
+    by = c("county_geono", "road_category")
   ) |>
   dplyr::mutate(
     measured_percentage_of_traffic_work = round(100 * measured_traffic_work_mill_km / traffic_work_mill_km, 0)
   ) |>
   dplyr::select(
-    county_id,
+    county_geono,
     road_category,
     measured_traffic_work_mill_km,
     measured_percentage_of_traffic_work
+  ) |>
+  dplyr::arrange(
+    county_geono
   )
 
-# Traffic work stats
-# traffic_work_stats <-
-#   traffic_work_per_link |>
-#   dplyr::summarise(
-#     traffic_work_mill_km = sum(traffic_work_km) / 1e6,
-#     .by = c(county_id, road_category)
-#   ) |>
-#   dplyr::select(
-#     county_id = county_id_single,
-#     road_category,
-#     traffic_work_mill_km
-#   )
-#
-# readr::write_rds(
-#   traffic_work,
-#   file = "new_vti/traffic_work_2023.rds"
-# )
-
-{
 county_table <-
   trp_for_vti_chosen |>
   dplyr::summarise(
     n_trp = n(),
-    .by = c(county_no, road_category)
-  ) |>
-  dplyr::rename(
-    county_id = county_no
+    .by = c(county_geono, road_category)
   ) |>
   dplyr::left_join(
     counties,
-    by = dplyr::join_by(county_id == county_number)
+    by = dplyr::join_by(county_geono)
   ) |>
   dplyr::select(
-    county_id,
+    county_geono,
     county_name,
     road_category,
     n_trp
   ) |>
   dplyr::left_join(
-    traffic_work,
-    by = dplyr::join_by(county_id, road_category)
+    traffic_work_stats,
+    by = dplyr::join_by(county_geono, road_category)
   ) |>
   dplyr::left_join(
     measured_traffic_work,
-    by = dplyr::join_by(county_id, road_category)
+    by = dplyr::join_by(county_geono, road_category)
   )
 
 readr::write_rds(
