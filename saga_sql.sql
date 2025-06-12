fd4c911071715bb34c633f446aeebf741cfa897a
Snorre Hansen <snorre.hansen@vegvesen.no>
2025-06-12 14:21:46 +0200

    Trino query example.

--- saga_sql.sql
+++ saga_sql.sql
@@ -1,13 +1,23 @@
 -- !preview conn=DBI::dbConnect(RSQLite::SQLite())
 
-select
-    trafikkmelding_nokkel,
-    hendelse_type,
-    hendelse_notat note,
-    hendelse_beskrivelse description,
-    date(hendelse_start_tidspunkt) date_start,
-    date(hendelse_slutt_tidspunkt) date_end,
-    hendelse_alle_kjoretoy_flagg all_vehicles
-from "trafikkmelding"
-where date(hendelse_start_tidspunkt) between date('2025-02-01') and date('2025-03-01')
-limit 15;
+-- SQL flavour: Trino
+-- https://trino.io/docs/current/language.html
+
+select *
+from
+(
+  select
+      trafikkmelding_nokkel,
+      hendelse_type,
+      hendelse_notat note,
+      hendelse_beskrivelse description,
+      date(hendelse_start_tidspunkt) date_start,
+      date(hendelse_slutt_tidspunkt) date_end,
+      date_diff('minute', hendelse_start_tidspunkt, hendelse_slutt_tidspunkt) duration,
+      hendelse_alle_kjoretoy_flagg all_vehicles
+  from silver.s_hbt.trafikkmelding
+  where date(hendelse_start_tidspunkt) between date('2025-02-01') and date('2025-03-01')
+)
+as events
+where duration > 60*24;
+
