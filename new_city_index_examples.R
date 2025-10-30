1a92120eb2d24ca9af5db0c2c3ef7dcc38dcc13a
Snorre Hansen <snorre.hansen@vegvesen.no>
2025-10-30 12:32:28 +0100

    Remove unused code for TRD example.

--- new_city_index_examples.R
+++ new_city_index_examples.R
@@ -100,148 +100,20 @@ source("new_city_index_examples_calculate.R")
 
 
 # Trondheim ----
-city_number <- "960"
-present_year <- 2025
-index_month <- 4
-source("set_time_references.R")
-
-
-## TRPs ----
-this_citys_trps_all_adt_final <-
-  readr::read_rds(
-    file = paste0(
-      "index_trp_metadata/trp_",
-      city_number,
-      ".rds"
-    )
-  ) |>
-  dplyr::filter(
-    stringr::str_sub(road_category_and_number, 1, 1) != "K"
-  ) |>
-  dplyr::select(
-    trp_id,
-    name,
-    road_reference,
-    municipality_name,
-    #lat, lon,
-    adt, year_aadt, adt_ref
-  )
-
-
-## Link population ----
-# Made in script city_link_population.R
-links_trondheim <-
-  readr::read_rds(
-    "traffic_link_pop/links_trondheim.rds"
-  )
-
-population_size <- nrow(links_trondheim)
-
-trp_weights <-
-  links_trondheim |>
-  sf::st_drop_geometry() |>
-  dplyr::filter(
-    !is.na(point_id)
-  ) |>
-  dplyr::select(
-    trp_id = point_id,
-    tw,
-    length_m
-  ) |>
-  dplyr::mutate(
-    tw = base::round(tw / 1000),
-    length_m = base::round(length_m)
-  )
-
-missing <-
-  this_citys_trps_all_adt_final |>
-  dplyr::filter(
-    !(trp_id %in% trp_weights$trp_id)
-  )
-# Some are outside urban area, some are missing from links
-
-
-## MDT ----
-mdt_filtered <-
-  readr::read_rds(
-    paste0(
-      "data_indexpoints_tidy/mdt_",
-      city_number,
-      ".rds"
-    )
-  )
-
-# To get the mdt_validated
 {
-  source("exclude_trp_mdts_list.R")
-
-  mdt_validated <-
-    mdt_validated |>
-    dplyr::inner_join(
-      # "inner" works as a filter here!
-      trp_weights,
-      by = dplyr::join_by(trp_id)
-    )
+  city_number <- "960"
+  links_in_area <- readr::read_rds("traffic_link_pop/links_trondheim.rds")
 }
 
-trp_mdt_ok_refyear <-
-  mdt_validated |>
-  dplyr::filter(
-    trp_id %in% links_trondheim$point_id
-  ) |>
-  filter_mdt(reference_year) |>
-  purrr::pluck(1)
+source("new_city_index_examples_prepare.R")
+source("new_city_index_examples_calculate.R")
 
-mdt_yearly <-
-  mdt_validated |>
-  dplyr::filter(
-    trp_id %in% trp_mdt_ok_refyear,
-    coverage >= 50,
-    length_quality >= 98.5
-  ) |>
-  dplyr::group_by(
-    trp_id,
-    year
-  ) |>
-  dplyr::summarise(
-    n_months = n(),
-    mean_mdt = base::mean(mdt) |> base::floor(),
-    .groups = "drop"
-  ) |>
-  dplyr::filter(
-    n_months >= 9
-  ) |>
-  dplyr::inner_join(
-    trp_weights,
-    by = dplyr::join_by(trp_id)
-  )
-
-n_trp_per_year <-
-  mdt_yearly |>
-  dplyr::summarise(
-    n_trp = n(),
-    .by = year
-  ) |>
-  dplyr::arrange(
-    year
-  )
-
-## Index calculation ----
-all_rolling_indices_old <- calculate_all_rolling_indices_old()
-
-{
-  tictoc::tic()
-  all_rolling_indices_new <- calculate_all_rolling_indices_tw(population_size)
-  tictoc::toc()
-}
-
-list(
-  all_rolling_indices_old,
-  all_rolling_indices_new
-) |>
-  readr::write_rds(
-    "representativity/new_index_examples_trondheim.rds"
-  )
+# missing <-
+#   this_citys_trps_all_adt_final |>
+#   dplyr::filter(
+#     !(trp_id %in% trp_weights$trp_id)
+#   )
+# Some are outside urban area (1), some are missing from links (6 on K-roads)
 
 
 # Nord-Jæren ----
