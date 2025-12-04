c759246955e4878707f37196e73738032868ba85
Snorre Hansen <snorrh@gmail.com>
2025-12-04 11:04:38 +0100

    City index 25-11 9/12 WIP

--- city_index_to_excel.R
+++ city_index_to_excel.R
@@ -5,7 +5,8 @@ if(city_number %in% c(19954, 19955, 20952)){
   list(
     punkt_adt = trp_info_adt,
     punktindeks_maned = trp_index_monthly_wide,
-    byindeks_aarlig = city_index_yearly_all#,
+    byindeks_aarlig = city_index_yearly_all,
+    byindeks_hittil = city_index_yearly_all_so_far#,
     #by_glid_indeks = all_rolling_indices
   ) |>
     writexl::write_xlsx(
@@ -40,8 +41,8 @@ if(city_number == 960){
     punkt_adt = this_citys_trps_all_adt_final_index,
     punktindeks_maned = trp_index_monthly_wide,
     byindeks_aarlig = city_index_yearly_all,
-    by_glid_indeks = all_rolling_indices,
-    byindeks_hittil = city_index_so_far_all
+    byindeks_hittil = city_index_so_far_all,
+    by_glid_indeks = all_rolling_indices
   ) |>
     writexl::write_xlsx(
       path = paste0(
