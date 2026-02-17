48cbfa90435276c6da3b96c197b6259c3e895002
snohan <snorrh@gmail.com>
2026-02-17 14:25:01 +0100

    Season index Tromsø

--- city_report_files/season_index.R
+++ city_report_files/season_index.R
@@ -197,63 +197,63 @@ index_season_chained <-
 
 
 # Removing summer
-index_without_summer <-
-  city_indexes_tidy |>
-  dplyr::filter(
-    period == "month",
-    year < 2024,
-    !(month %in% c(5:8))
-  ) |>
-  dplyr::summarise(
-    sum_base = sum(base_volume),
-    sum_calc = sum(calc_volume),
-    index_i = sum_calc / sum_base,
-    index_p_without_summer = 100 * (index_i - 1),
-    .by = year
-  )
+# index_without_summer <-
+#   city_indexes_tidy |>
+#   dplyr::filter(
+#     period == "month",
+#     year < 2024,
+#     !(month %in% c(5:8))
+#   ) |>
+#   dplyr::summarise(
+#     sum_base = sum(base_volume),
+#     sum_calc = sum(calc_volume),
+#     index_i = sum_calc / sum_base,
+#     index_p_without_summer = 100 * (index_i - 1),
+#     .by = year
+#   )
 
-index_just_summer <-
-  city_indexes_tidy |>
-  dplyr::filter(
-    period == "month",
-    year < 2024,
-    month %in% c(5:8)
-  ) |>
-  dplyr::summarise(
-    sum_base = sum(base_volume),
-    sum_calc = sum(calc_volume),
-    index_i = sum_calc / sum_base,
-    index_p_summer = 100 * (index_i - 1),
-    .by = year
-  )
+# index_just_summer <-
+#   city_indexes_tidy |>
+#   dplyr::filter(
+#     period == "month",
+#     year < 2024,
+#     month %in% c(5:8)
+#   ) |>
+#   dplyr::summarise(
+#     sum_base = sum(base_volume),
+#     sum_calc = sum(calc_volume),
+#     index_i = sum_calc / sum_base,
+#     index_p_summer = 100 * (index_i - 1),
+#     .by = year
+#   )
 
 # Compare
-index_seasons <-
-  year_to_date_from_api |>
-  dplyr::select(
-    year,
-    index_p_all_year = index_p
-  ) |>
-  dplyr::left_join(
-    index_without_summer,
-    by = join_by(year)
-  ) |>
-  dplyr::left_join(
-    index_just_summer,
-    by = join_by(year)
-  ) |>
-  dplyr::select(
-    year,
-    index_p_all_year,
-    index_p_without_summer,
-    index_p_summer
-  ) |>
-  dplyr::mutate(
-    dplyr::across(
-      tidyselect::starts_with("index_p"),
-      ~ round(.x, 1)
-    )
-  )
+# index_seasons <-
+#   year_to_date_from_api |>
+#   dplyr::select(
+#     year,
+#     index_p_all_year = index_p
+#   ) |>
+#   dplyr::left_join(
+#     index_without_summer,
+#     by = join_by(year)
+#   ) |>
+#   dplyr::left_join(
+#     index_just_summer,
+#     by = join_by(year)
+#   ) |>
+#   dplyr::select(
+#     year,
+#     index_p_all_year,
+#     index_p_without_summer,
+#     index_p_summer
+#   ) |>
+#   dplyr::mutate(
+#     dplyr::across(
+#       tidyselect::starts_with("index_p"),
+#       ~ round(.x, 1)
+#     )
+#   )
 
 # readr::write_csv2(
 #   index_seasons,
@@ -262,23 +262,23 @@ index_seasons <-
 
 
 # Chaining
-chained_index_season <-
-  city_indexes_tidy |>
-  dplyr::filter(
-    period == "month",
-    year < 2024
-  ) |>
-  dplyr::mutate(
-    summer = month %in% c(5:8)
-  ) |>
-  dplyr::summarise(
-    index_i = sum(calc_volume) / sum(base_volume),
-    .by = c(year, summer),
-  ) |>
-  dplyr::summarise(
-    chained_index_i = prod(index_i),
-    .by = c(summer)
-  ) |>
-  dplyr::mutate(
-    chained_index_p = 100 * (chained_index_i - 1),
-  )
+# chained_index_season <-
+#   city_indexes_tidy |>
+#   dplyr::filter(
+#     period == "month",
+#     year < 2024
+#   ) |>
+#   dplyr::mutate(
+#     summer = month %in% c(5:8)
+#   ) |>
+#   dplyr::summarise(
+#     index_i = sum(calc_volume) / sum(base_volume),
+#     .by = c(year, summer),
+#   ) |>
+#   dplyr::summarise(
+#     chained_index_i = prod(index_i),
+#     .by = c(summer)
+#   ) |>
+#   dplyr::mutate(
+#     chained_index_p = 100 * (chained_index_i - 1),
+#   )
